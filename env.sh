ee9a979d8a3b4f947b3475d61a292a1234f29402
Teagan glenn <teagan.m.glenn@gmail.com>
2021-01-20 23:17:24 -0700

    Setup scripts and templates

---
+++ env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export HOSTNAME=
+export MQTTHOST=mqtt.home.prettybaked.com
+export MQTTPORT=1883
+export CLIENTID=
+export LOCATION=
+
+export TEAGAN_MAC=
+export ALAN_MAC=
+
+export WIFISSID=
+export WIFIPSK=
---
+++ pi-setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+SETUP_COMPLETE_FILE=/var/.pisetup
+if [[ -f "$FILE" ]]; then
+    echo "Setup has run, skipping"
+    exit 0
+fi
+
+# Setup Rapsi-Config
+/usr/lib/raspi-config/init_resize.sh
+
+sudo aspi-config nonint do_hostname "$HOSTNAME"
+sudo raspi-config nonint do_ssh 1
+sudo raspi-config nonint do_wifi_country "US"
+sudo raspi-config nonint do_wifi_passphrase "$WIFISSID" "$WIFIPSK"
+
+# Update and upgrade
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+
+# Install mosquito
+# get repo key
+wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
+
+#add repo
+sudo apt-key add mosquitto-repo.gpg.key
+
+#download appropriate lists file
+cd /etc/apt/sources.list.d/
+sudo wget http://repo.mosquitto.org/debian/mosquitto-buster.list
+
+#update caches and install
+sudo apt-cache search mosquitto
+sudo apt-get update
+sudo apt-get install -f libmosquitto-dev mosquitto mosquitto-clients libmosquitto1
+
+# Install package
+sudo apt-get install -y vim git pi-bluetooth
+
+# Install monitor
+cd ~
+git clone https://github.com/constructorfleet/monitor.git
+git clone https://github.com/constructprfleet/monitor-setup-script.git
+cd monitor/
+
+
+sudo touch SETUP_COMPLETE_FILE
